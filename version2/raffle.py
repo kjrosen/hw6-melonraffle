2e9c3d9678458a4767199c20c4e660994b7cda67
Kaitlin Rosen <kayejrosen@gmail.com>
2022-03-23 18:37:39 -0700

    Created Independent raffle.py

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,16 @@
 """Randomly pick customer and print customer info"""
+from customers import Customer
+from customers import get_customers_from_file
+from random import choice
 
-# Add code starting here
+def raffle(customers):
+    '''pulls a randomly selected winner from the customer list'''
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+    chosen_customer = choice(customers)
+    message = f"""{chosen_customer.name} has won the raffle!
+    Reach them at {chosen_customer.email}"""
+    
+    return message
+
+contestants = get_customers_from_file("customers.txt")
+print(raffle(contestants))
